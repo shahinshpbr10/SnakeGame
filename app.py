8a7accdd7ebf0e9498221d140e23a222ef78d371
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2024-03-19 20:58:25 +0530

    initial Commit of snake game

---
+++ app.py
@@ -0,0 +1,23 @@
+from  tkinter import *
+import random
+
+GAME_WIDTH=700
+GAME_HEIGHT=700
+SPEED=50
+SPACE_SIZE=50
+BODY_PART=3
+SNAKE_COLOR="#00FF00"
+FOOD_COLOR="#FF0000"
+
+class snake():
+    pass
+class food():
+    pass
+def nextTurn():
+    pass
+def changeDirection():
+    pass
+def checkCollision():
+    pass
+def gameOver():
+    pass
