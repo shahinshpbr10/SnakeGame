a1fed69b8d43cacd407ba06a49c12640e30c2018
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2024-03-19 21:13:58 +0530

    window label and canvas created for game

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ SPACE_SIZE=50
 BODY_PART=3
 SNAKE_COLOR="#00FF00"
 FOOD_COLOR="#FF0000"
+BACKGROUND_COLOR="#000000"
 
 class snake():
     pass
@@ -21,3 +22,13 @@ def checkCollision():
     pass
 def gameOver():
     pass
+window=Tk()
+window.title("Snake Game")
+window.resizable(False,False)
+score=0
+direction='down'
+label=Label(window,text='Score{}'.format(score),font=('consolas', 40))
+label.pack()
+canvas=Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+canvas.pack()
+window.mainloop()
